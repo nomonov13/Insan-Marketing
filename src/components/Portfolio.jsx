98c82eeb7bebef8066681dd4573e5bba56ffadd0
shohjahon-asqarov <shohjahonasqarov.web@gmail.com>
2025-04-01 16:44:12 +0500

    fix portfolio footer cta

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,91 +6,91 @@ import icon2 from '../assets/icon2.svg'
 const Portfolio = () => {
   return (
     <div>
-      <div className='container'>
+      <div className='container portfolio py-10'>
         <h1 className='text-center text-5xl text-dark-blue mt-32 mb-12'>Portfolio</h1>
-        <ul className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6'>
+        <ul className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-20'>
           <li>
             <img src={icon} alt="" />
-           </li>
+          </li>
 
           <li>
             <img src={icon2} alt="" />
-           </li>
+          </li>
 
-           <li>
-           <img src={icon} alt="" />
-           </li>
-
-           <li>
-            <img src={icon2} alt="" />
-           </li>
-
-           <li>
+          <li>
             <img src={icon} alt="" />
-           </li>
+          </li>
 
-           <li>
+          <li>
             <img src={icon2} alt="" />
-           </li>
+          </li>
 
-           <li>
+          <li>
             <img src={icon} alt="" />
-           </li>
+          </li>
 
-           <li>
+          <li>
             <img src={icon2} alt="" />
-           </li>
+          </li>
 
-           <li>
+          <li>
             <img src={icon} alt="" />
-           </li>
+          </li>
 
-           <li>
+          <li>
             <img src={icon2} alt="" />
-           </li>
+          </li>
 
-           <li>
+          <li>
             <img src={icon} alt="" />
-           </li>
+          </li>
 
-           <li>
+          <li>
             <img src={icon2} alt="" />
-           </li>
+          </li>
 
-           <li>
+          <li>
             <img src={icon} alt="" />
-           </li>
+          </li>
 
-           <li>
+          <li>
             <img src={icon2} alt="" />
-           </li>
+          </li>
 
-           <li>
+          <li>
             <img src={icon} alt="" />
-           </li>
+          </li>
 
-           <li>
+          <li>
             <img src={icon2} alt="" />
-           </li>
+          </li>
 
-           <li>
+          <li>
             <img src={icon} alt="" />
-           </li>
+          </li>
 
-           <li>
+          <li>
             <img src={icon2} alt="" />
-           </li>
+          </li>
 
-           <li>
+          <li>
             <img src={icon} alt="" />
-           </li>
+          </li>
 
-           <li>
+          <li>
             <img src={icon2} alt="" />
-           </li>
-           
+          </li>
+
+          <li>
+            <img src={icon} alt="" />
+          </li>
+
+          <li>
+            <img src={icon2} alt="" />
+          </li>
+
         </ul>
-        
+
       </div>
     </div>
   )
--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -2,25 +2,24 @@ import React from 'react'
 
 const Questions = () => {
   return (
-    <section className='bg-dark-blue'>
-        <div className="container flex  space-x-48  items-center  p-8">
-            <div className=' items-center space-y-3'>
-                <h1 className='text-white text-5xl   font-semibold' >
-                Savolingiz bormi?
-                </h1>
-                <p className='text-dark-gray text-sm '>Ma'lumotlaringizni qoldiring va biz tez orada siz bilan <br /> bog’lanamiz!</p>
-            </div>
-           <div className=' space-y-4 ' >
-           <div >
-           <input className='w-64 h-10 border-light-gray border-2 rounded-2xl text-center text-white' type="text" placeholder ='F.I.O' />
-           </div>
-           <div>
-           <input className='w-64 h-10 border-light-gray border-2 rounded-2xl text-center text-white' type="number" placeholder ='Telefon raqamingiz' />
-           </div>
-
-           <button className='btn-white'>Yuborish</button>
-           </div>
+    <section className='bg-dark-blue py-20'>
+      <div className="container flex  items-center justify-center space-x-48">
+        <div className='items-center space-y-3'>
+          <h1 className='text-white text-5xl   font-semibold' >
+            Savolingiz bormi?
+          </h1>
+          <p className='text-dark-gray text-sm '>Ma'lumotlaringizni qoldiring va biz tez orada siz bilan <br /> bog’lanamiz!</p>
         </div>
+        <div className='space-y-4' >
+          <div >
+            <input className='w-64 h-10 border-light-gray border-2 rounded-2xl text-center text-white' type="text" placeholder='F.I.O' />
+          </div>
+          <div>
+            <input className='w-64 h-10 border-light-gray border-2 rounded-2xl text-center text-white' type="number" placeholder='Telefon raqamingiz' />
+          </div>
+          <button className='btn-white'>Yuborish</button>
+        </div>
+      </div>
     </section>
   )
 }
