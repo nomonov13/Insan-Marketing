7234fa4be460ebddaba39dde1ca66197dfa460fc
nomonov13 <nomonovshohjahon26@gmail.com>
2025-04-01 16:37:31 +0500

    fix: agency component

--- src/Components/Agency.jsx
+++ src/Components/Agency.jsx
@@ -9,7 +9,7 @@ const Agency = () => {
        </div>
        <div className='space-y-10'>
            <h1 className='text-5xl'>Коротко  <br /> о нас</h1>
-           <p className='text-black'>Biz brendlarni raqamli marketingini yuritamiz. Soddaroq <br /> qilib aytganda, internet orqali har qanday biznesning <br /> raqamli marketingini yurg'izib, ularga mijozlarini ko'payishi uchun xizmat qilamiz.</p>
+           <p className='text-black'>Biz brendlarni raqamli marketingini yuritamiz. Soddaroq <br /> qilib aytganda, internet orqali har qanday biznesning <br /> raqamli marketingini yurg'izib, ularga mijozlarini ko'payishi <br /> uchun xizmat qilamiz.</p>
            <p className='mt-9 text-black'><span > Maqsadimiz:</span> Insonlarga foyda beruvchi sifatli <br /> mahsulotlarni ommaga yetib borishida xizmat qilish!</p>
            <button className='btn-dark'>нАШИ УСЛУГИ</button>
        </div>
