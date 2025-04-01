cc69152f18b2639d8f09cb6936594b053943daca
kamola02007 <abduvaxobovakamola07@gmail.com>
2025-04-01 16:24:04 +0500

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Xizmatlar from './components/Xizmatlar'
+import Portfolio from './components/Portfolio'
 
 const App = () => {
   return (
     <>
-    
+    <main>
+      <Xizmatlar/>
+      <Portfolio/>
+    </main>
     </>
   )
 }
