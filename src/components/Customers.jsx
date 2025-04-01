366af6cf90252bb439d532c05b689ff33390cd15
sunnatullo54 <sultanovural28@gmail.com>
2025-04-01 18:14:39 +0500

    til

--- src/components/Customers.jsx
+++ src/components/Customers.jsx
@@ -2,40 +2,37 @@ import React from "react";
 import logo1 from "../assets/images/logo1.png";
 import logo2 from "../assets/images/logo2.png";
 import logo3 from "../assets/images/logo3.png";
+import { useTranslation } from "react-i18next";
 
 const Customers = () => {
+  const { t } = useTranslation();
   return (
     <div className="container px-6 py-[69px] mx-auto">
-      <h1 className="font-bold text-[60px] text-center">
-        Mijozlarning sharhlari
-      </h1>
+      <h1 className="font-bold text-[60px] text-center">{t("customers")}</h1>
 
       <div className="flex flex-wrap justify-center gap-[30px] mt-[80px]">
-        <div className="w-[414px] p-[40px] rounded-2xl shadow-md bg-white">
+        <div className="div w-[414px] p-[40px] rounded-2xl shadow-md bg-white">
           <p className="text-[14px] leading-[166%] text-gray-700">
-            Sizlarning jamoangiz bilan juda ham yaxshi ishlayapman. Ishingizga
-            mas'uliyat bilan yondashasizlar. Bundan keyin ham birgalikda ishlash
-            nasib qilsin. Ishlaringizga rivoj. Alloh sizlardan rozi bo'lsin.
+            {t("user1-commit")}
           </p>
-          <hr className="my-4 border-gray-300" />
-          <div className="flex items-center gap-3">
-            <img
-              src={logo1}
-              alt="Umar Najimov"
-              className="w-10 h-10 rounded-full"
-            />
-            <div>
-              <h2 className="text-[14px] font-medium">Umar Najimov</h2>
-              <p className="text-[12px] text-gray-600">
-                “Parfum Embassy” atirlar do‘koni
-              </p>
+          <div>
+            <hr className="my-4 border-gray-300" />
+            <div className="flex items-center gap-3">
+              <img
+                src={logo1}
+                alt="Umar Najimov"
+                className="w-10 h-10 rounded-full"
+              />
+              <div>
+                <h2 className="text-[14px] font-medium">{t("user1-name")}</h2>
+                <p className="text-[12px] text-gray-600">{t("user1-job")}</p>
+              </div>
             </div>
           </div>
         </div>
         <div className="div w-[414px] p-[40px] rounded-2xl shadow-md bg-white">
           <p className="text-[14px] leading-[166%] text-gray-700">
-            Zo'r ish olib borishadi, shartnomaga to'liq amal qilinadi, barcha
-            majburiyatlar bajariladi. Ishilarga omad.
+            {t("user2-commit")}
           </p>
           <div>
             <hr className="my-4 border-gray-300" />
@@ -46,18 +43,15 @@ const Customers = () => {
                 className="w-10 h-10 rounded-full"
               />
               <div>
-                <h2 className="text-[14px] font-medium">Sultonmurod Abdusalomov</h2>
-                <p className="text-[12px] text-gray-600">
-                “allOne” taksi platformasi asoschisi
-                </p>
+                <h2 className="text-[14px] font-medium">{t("user2-name")}</h2>
+                <p className="text-[12px] text-gray-600">{t("user2-job")}</p>
               </div>
             </div>
           </div>
         </div>
         <div className="div w-[414px] p-[40px] rounded-2xl shadow-md bg-white">
           <p className="text-[14px] leading-[166%] text-gray-700">
-            Barcha postlar o'z vaqtida chiqadi, kontent sifatli. Mas'uliyatli
-            agentlik, menga yoqdi.
+            {t("user3-commit")}
           </p>
           <div>
             <hr className="my-4 border-gray-300" />
@@ -68,10 +62,8 @@ const Customers = () => {
                 className="w-10 h-10 rounded-full"
               />
               <div>
-                <h2 className="text-[14px] font-medium">Abror Topilov</h2>
-                <p className="text-[12px] text-gray-600">
-                  “Dezintash” dezinfeksiya korxonasi rahbari
-                </p>
+                <h2 className="text-[14px] font-medium">{t("user3-name")}</h2>
+                <p className="text-[12px] text-gray-600">{t("user3-job")}</p>
               </div>
             </div>
           </div>
